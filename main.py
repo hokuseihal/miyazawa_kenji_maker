eee38d9316b437c9c3b85320f9e62505aeffbc20
wadanaoki <hokutoumic@gmail.com>
2018-12-30 14:45:35 +0900

    fix key error

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def miyazawa(usepart=True):
                 if None!=re.match(r'\d{3}',line):
                     divided_text+=t.tokenize(line)[2:]
     # only test end----------
-
-    flow = [((str(str(part).split()[0])), (str(str(part).split()[1]).split(',')[0])) for part in divided_text]
+    list=['フィラー', '副詞', '助動詞', '助詞', '動詞', '名詞', '形容詞', '感動詞', '接続詞', '接頭詞', '記号', '連体詞']
+    flow = [((str(str(part).split()[0])), (str(str(part).split()[1]).split(',')[0])) for part in divided_text if (str(str(part).split()[1]).split(',')[0]) in list]
     table = flow2table.flow2table(flow)
     flow_ided = [table.index(drop) for drop in flow]
 
