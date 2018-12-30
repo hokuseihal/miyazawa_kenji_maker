11e666d143f0fa04ecf0f694c5c7e912c9450761
hokuseihal <35138128+hokuseihal@users.noreply.github.com>
2018-12-30 14:52:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def miyazawa(usepart=True):
         rawtext = f.read().lower()
     text += re.sub(r"<.*?>|（.*?）", "", rawtext)
     # only test end----------
-
+    t=Tokenizer()
+    divided_text=t.tokenize(text)
     list=['フィラー', '副詞', '助動詞', '助詞', '動詞', '名詞', '形容詞', '感動詞', '接続詞', '接頭詞', '記号', '連体詞']
     flow = [((str(str(part).split()[0])), (str(str(part).split()[1]).split(',')[0])) for part in divided_text if (str(str(part).split()[1]).split(',')[0]) in list]
     table = flow2table.flow2table(flow)
